6c7b066bbb6a39d776a44cc5821b8d75174628a9
Koloma Soro <koloma952009@gmail.com>
2019-06-19 19:32:56 +0000

    Show heroes list and display messages

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,14 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+  { id: 1, name: 'React Native' },
+  { id: 2, name: 'Angular' },
+  { id: 3, name: 'Laravel' },
+  { id: 4, name: 'Symfony' },
+  { id: 5, name: 'Vue' },
+  { id: 6, name: 'Ionic' },
+  { id: 7, name: 'NodeJS' },
+  { id: 8, name: 'JavaScript' },
+  { id: 9, name: 'PHP' },
+  { id: 10, name: 'Flutter' }
+]
